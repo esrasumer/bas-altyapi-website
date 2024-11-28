adf2119a59cd3f2c2ef897eff61736191f2d2485
esrasumer <esrasumer28@gmail.com>
2024-11-28 20:31:27 +0300

    contact updated

--- app/components/contact/Contact.jsx
+++ app/components/contact/Contact.jsx
@@ -1,39 +1,59 @@
-"use client"
+'use client'
 
-import Image from 'next/image';
-import React, { useState } from 'react';
-import ContactImg from '../../../public/assets/about.jpg';
-import Map from '../Map';
+import Image from 'next/image'
+import React, { useState } from 'react'
+import ContactImg from '../../../public/assets/about.jpg'
+import Map from '../Map'
 import './style.css'
 
 const Contact = () => {
 
+  async function handleForm(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "977647fa-d2da-47b1-908b-379e76d485c5");
+
+    const object = Object.fromEntries(formData);
+    const json = JSON.stringify(object);
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json"
+        },
+        body: json
+    });
+    const result = await response.json();
+    if (result.success) {
+        console.log(result);
+    }
+}
+
   const [formData, setFormData] = useState({
     name: '',
     phoneNumber: '',
-    email: '',
-  });
+    email: ''
+  })
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
+  const handleChange = e => {
+    const { name, value } = e.target
     setFormData({
       ...formData,
       [name]: value
-    });
-  };
+    })
+  }
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log('Form data:', formData);
-
+  const handleSubmit = e => {
+    e.preventDefault()
+    console.log('Form data:', formData)
   }
 
   return (
     <div id='contact' className='w-full lg:h-screen'>
       <div className='max-w-[1240px] m-auto px-2 py-16 w-full '>
-        <p className='text-xl tracking-widest text-[#5651e5]'>
-          BİZE ULAŞIN
-        </p>
+        <p className='text-xl tracking-widest text-[#5651e5]'>BİZE ULAŞIN</p>
         <div className='grid lg:grid-cols-5 gap-8'>
           <div className='col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4'>
             <div className='lg:p-4 h-full '>
@@ -55,11 +75,15 @@ const Contact = () => {
           <div className='col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4'>
             <div className='p-4'>
               <form
-                onSubmit={handleSubmit}
+                action='https://api.web3forms.com/submit'
+                method='POST'
+                onSubmit={handleForm}
               >
                 <div className='grid md:grid-cols-2 gap-4 w-full py-2'>
                   <div className='flex flex-col'>
-                    <label htmlFor='name' className='text-sm py-2'>İSİM-SOYİSİM</label>
+                    <label htmlFor='name' className='text-sm py-2'>
+                      İSİM-SOYİSİM
+                    </label>
                     <input
                       className='border-2 rounded-lg p-3 flex border-gray-300'
                       type='text'
@@ -67,13 +91,13 @@ const Contact = () => {
                       id='name'
                       value={formData.name}
                       onChange={handleChange}
-                      pattern="^[A-Za-zÇçĞğİıÖöŞşÜü\s]{1,50}$"
-                      title="Lütfen yalnızca harfler kullanın"
+                      pattern='^[A-Za-zÇçĞğİıÖöŞşÜü\s]{1,50}$'
+                      title='Lütfen yalnızca harfler kullanın'
                       required
                     />
                   </div>
                   <div className='flex flex-col'>
-                    <label htmlFor="phoneNumber" className='text-sm py-2'>
+                    <label htmlFor='phoneNumber' className='text-sm py-2'>
                       TELEFON NUMARASI
                     </label>
                     <input
@@ -84,13 +108,15 @@ const Contact = () => {
                       value={formData.phoneNumber}
                       onChange={handleChange}
                       title='lütfen telefon numarası girin'
-                      pattern="^\d{10,15}$"
+                      pattern='^\d{10,15}$'
                       required
                     />
                   </div>
                 </div>
                 <div className='flex flex-col py-2'>
-                  <label htmlFor="email" className='text-sm py-2'>E-MAİL</label>
+                  <label htmlFor='email' className='text-sm py-2'>
+                    E-MAİL
+                  </label>
                   <input
                     className='border-2 rounded-lg p-3 flex border-gray-300'
                     type='email'
@@ -117,7 +143,10 @@ const Contact = () => {
                     name='message'
                   ></textarea>
                 </div>
-                <button type="submit" className='w-full p-4 text-logogrowhitund-100 mt-4 sendButton'>
+                <button
+                  type='submit'
+                  className='w-full p-4 text-logogrowhitund-100 mt-4 sendButton'
+                >
                   GÖNDER
                 </button>
               </form>
@@ -126,7 +155,7 @@ const Contact = () => {
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default Contact;
+export default Contact
