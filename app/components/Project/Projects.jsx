a60d909940a046ed53dcc373885eabc7586eaadf
esrasumer <esrasumer28@gmail.com>
2024-12-01 23:56:18 +0300

    added slider

--- app/components/Project/Projects.jsx
+++ app/components/Project/Projects.jsx
@@ -1,11 +1,20 @@
 import React from 'react'
 import Image from 'next/image'
-import projectImage from '../../../public/assets/projectImage.webp'
 import Imagecar from '../../../public/assets/ProjectImage/Image1.jpeg'
 import Tunel from '../../../public/assets/ProjectImage/Image2.jpeg'
 import Cukur from '../../../public/assets/ProjectImage/Image3.jpeg'
+import Bahce from '../../../public/assets/ProjectImage/Image4.jpeg'
 import { Carousel } from 'antd'
+import './style.css'
 
+const contentStyle = {
+  margin: 0,
+  height: '640px',
+  color: '#fff',
+  lineHeight: '600px',
+  textAlign: 'center',
+  background: '#364d79'
+}
 function Projects () {
   return (
     <div id='projects' className='w-full'>
@@ -17,44 +26,48 @@ function Projects () {
           Hizmetlerimiz hakkında daha fazla bilgi edinmek isterseniz, bize
           ulaşmanız yeterli.
         </h2>
-        <Carousel autoplay>
-          <div className='grid md:grid-cols-2 gap-8'>
-            <h3 className='contentStyle'>
-              <Image
-                src={projectImage}
-                alt='/'
-                width='500'
-                height='500'
-                className='cursor-pointer rounded-full'
-              />
+
+        <Carousel className='slider-wrapper' arrows infinite={false}>
+          <div>
+            <h3 style={contentStyle}>
+              <div className='slider'>
+                <Image
+                  src={Imagecar}
+                  alt='/'
+                  width='300'
+                  height='300'
+                  className='slide'
+                  padding='300'
+                />{' '}
+                <Image
+                  src={Tunel}
+                  alt='/'
+                  width='440'
+                  height='440'
+                  className='slide'
+                />{' '}
+              </div>
             </h3>
           </div>
           <div>
-            <Image
-              src={Imagecar}
-              alt='/'
-              width='500'
-              height='500'
-              className='contentStyle '
-            />{' '}
-          </div>
-          <div>
-            <Image
-              src={Tunel}
-              alt='/'
-              width='500'
-              height='500'
-              className='contentStyle'
-            />{' '}
-          </div>
-          <div>
-            <Image
-              src={Cukur}
-              alt='/'
-              width='500'
-              height='500'
-              className='contentStyle'
-            />{' '}
+            <h3 style={contentStyle}>
+              <div className='slider'>
+                <Image
+                  src={Cukur}
+                  alt='/'
+                  width='500'
+                  height='500'
+                  className='slide'
+                />{' '}
+                <Image
+                  src={Bahce}
+                  alt='/'
+                  width='500'
+                  height='500'
+                  className='slide'
+                />
+              </div>
+            </h3>
           </div>
         </Carousel>
       </div>
