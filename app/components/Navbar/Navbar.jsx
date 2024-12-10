307ff8fc9603242b6661253bff4050700c8660d4
esrasumer <esrasumer28@gmail.com>
2024-12-10 23:54:12 +0300

    solved debug

--- app/components/Navbar/Navbar.jsx
+++ app/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 'use client'
 
 import React, { useState } from 'react'
-import Image from 'next/image'
+// import Image from 'next/image'
 import Link from 'next/link'
 import { RxHamburgerMenu } from 'react-icons/rx'
 import { RiCloseLargeFill } from 'react-icons/ri'
