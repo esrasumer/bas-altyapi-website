38c5584883580d8be507b5cd97dca46678e69c0c
esrasumer <esrasumer28@gmail.com>
2024-12-15 13:44:53 +0300

    project updated and  Head component deleted

--- app/page.tsx
+++ app/page.tsx
@@ -4,14 +4,12 @@ import Projects from './components/Project/Projects';
 import Contact from './components/contact/Contact';
 import HomePage from './components/home/HomePage'
 import Footer from './components/footer/Footer';
-import HeadPage from './components/Head/HeadPage'
 import TopBar from './components/TopBar/TopBar'
 
 export default function Home() {
   return (
     <div>
       <TopBar />
-      <HeadPage />
       <Navbar />
       <HomePage />
       <About />
