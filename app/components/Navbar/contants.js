184b7be84bc6de4bccb9af4741b86a65d44188ec
esrasumer <esrasumer28@gmail.com>
2024-12-17 01:16:49 +0300

    ...

--- app/components/Navbar/contants.js
+++ app/components/Navbar/contants.js
@@ -1,6 +1,6 @@
 export const NAVBAR_LINKS = [
-  { href: '/', label: 'deneme' },
-  { href: '/#about', label: 'deneme2' },
+  { href: '/', label: 'Anasayfa' },
+  { href: '/#about', label: 'Hakkımızda' },
   { href: '/#services', label: 'Hizmetlerimiz' },
   { href: '/#projects', label: 'Projelerimiz' },
   { href: '/#resume', label: 'Kariyer' },
